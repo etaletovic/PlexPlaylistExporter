5abdc57aa8398f8a07c8670c543599ac0274fc12
Emir Taletovic <5982655+etaletovic@users.noreply.github.com>
2022-08-15 22:37:21 -0700

    OfflinePlaylistService can return basic info for offline playlists

--- PlexPlaylistExporter.Core/Services/OfflinePlaylistService.cs
+++ PlexPlaylistExporter.Core/Services/OfflinePlaylistService.cs
@@ -10,11 +10,42 @@ namespace PlexPlaylistExporter.Core.Services
 {
     public class OfflinePlaylistService : IPlaylistService
     {
+        private static readonly string OfflinePlaylistsLocation = @$"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Plexamp\Plexamp\Offline\";
+
         public async Task<IEnumerable<Playlist>> GetPlaylists()
         {
             await Task.CompletedTask;
 
-            return Enumerable.Range(1, 10).Select(x => new Playlist { Id = x.ToString(), Name = $"Playlist {x}" });
+            if (!Directory.Exists(OfflinePlaylistsLocation))
+                return Enumerable.Empty<Playlist>();
+
+            return Directory.GetDirectories(OfflinePlaylistsLocation)
+                            .Select(x => new DirectoryInfo(x))
+                            .Select( x =>
+                            {
+                                var thumbnail = x.GetFiles().FirstOrDefault(f => f.Name.Equals("thumb.jpg", StringComparison.OrdinalIgnoreCase));
+                                return new Playlist
+                                {
+                                    Id = x.Name,
+                                    Name = $"Unknown Playlist",
+                                    ThumbUrl = thumbnail is null ? string.Empty : thumbnail.FullName,
+                                    Tracks = GetTracks(x.Name)
+                                };
+                            });
+        }
+
+        private List<Track> GetTracks(string playlistId)
+        {
+
+            var playlist = Path.Combine(OfflinePlaylistsLocation, playlistId);
+            var index = Path.Combine(playlist, "index.json");
+
+            if (!Directory.Exists(playlist) || !File.Exists(index))
+                return Enumerable.Empty<Track>().ToList();
+
+            return Enumerable.Empty<Track>().ToList();
+
+            var files = Directory.GetFiles(playlist);
         }
     }
 }
