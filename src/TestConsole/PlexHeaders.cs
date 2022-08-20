a8cfdf8a99bd9541cea32b94950549a53cdb9ad2
Emir Taletovic <5982655+etaletovic@users.noreply.github.com>
2022-08-20 02:41:23 -0700

    Collected headers in a class

---
+++ src/TestConsole/PlexHeaders.cs
@@ -0,0 +1,7 @@
+﻿public static class PlexHeaders
+{
+    public const string XPlexProduct = "X-Plex-Product";
+    public const string XPlexClientIdentifier = "X-Plex-Client-Identifier";
+    public const string XPlexToken = "X-Plex-Token";
+    public const string Strong = "strong";
+}
