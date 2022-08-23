b4ee0df4c2e01c1e1c7bc393126a79397dc5b7e7
Emir Taletovic <5982655+etaletovic@users.noreply.github.com>
2022-08-22 21:37:16 -0700

    Clean up DPAPI extensions

--- src/TestConsole/DataProtectionApi.cs
+++ src/TestConsole/DataProtectionApi.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -14,8 +12,7 @@ namespace TestConsole
 
             byte[] entropy = Encoding.ASCII.GetBytes(Assembly.GetExecutingAssembly().FullName);
             byte[] data = Encoding.ASCII.GetBytes(str);
-            string protectedData = Convert.ToBase64String(ProtectedData.Protect(data, entropy, DataProtectionScope.CurrentUser));
-            return protectedData;
+            return Convert.ToBase64String(ProtectedData.Protect(data, entropy, DataProtectionScope.CurrentUser));
         }
 
         public static string Unprotect(this string str)
@@ -24,8 +21,7 @@ namespace TestConsole
 
             byte[] protectedData = Convert.FromBase64String(str);
             byte[] entropy = Encoding.ASCII.GetBytes(Assembly.GetExecutingAssembly().FullName);
-            string data = Encoding.ASCII.GetString(ProtectedData.Unprotect(protectedData, entropy, DataProtectionScope.CurrentUser));
-            return data;
+            return Encoding.ASCII.GetString(ProtectedData.Unprotect(protectedData, entropy, DataProtectionScope.CurrentUser));
         }
     }
 }
